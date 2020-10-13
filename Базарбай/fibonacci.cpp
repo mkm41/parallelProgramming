c3f9f9a55b6f44c2b6750b706dffa31b3ccef930
mkm 41 <72242729+mkm41@users.noreply.github.com>
2020-10-13 21:20:33 +0600

    Add files via upload

--- Базарбай/fibonacci.cpp
+++ Базарбай/fibonacci.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<mutex>
 #include<thread>
+#define N 10
  
 unsigned long const hardware_threads=std::thread::hardware_concurrency();
 
@@ -16,70 +17,65 @@ class Fib {
 
 int Fib::operator ()(){
 	int temp=a0_;
-	a0_=a1_;
-	a1_=temp+a0_;
+	a0_ = a1_;
+	a1_ = temp + a0_;
 	return temp;
 }
 
-void evenfib(int n){
-	int summa1=0;
-	Fib evensum;
-		for(int i=0;i<n;i++){
-		if(i%2==0){
-			evensum();
-			continue;
+void ZhupTaq(int n){
+	Fib fib;
+	int j=1;
+	int t=2;
+	if (n == 2){
+		mu.lock();
+		std::cout<<"Indeksi taq bolatyn Fibonacci sandary: ";
+		mu.unlock();
+		for(int i=0;i<N;i++){
+			if(i%2==0){
+				std::cout<<"["<<j<<"]="<<fib()<<std::endl;
+				j+=2;
+			}
+			else
+				fib();
 		}
-		
-		summa1+=evensum();
+		std::cout<<std::endl;
 	}
-	std::cout<<"summa even fibonacci numbers-"<<summa1<<std::endl;
+	else{
+		mu.lock();
+		std::cout<<"Indeksi jup bolatyn Fibonacci sandary: ";
+		mu.unlock();
+		for(int i=0;i<N;i++){
+			if(i%2!=0){
+				std::cout<<"["<<t<<"]="<<fib()<<std::endl;
+				t+=2;
+			}
+			else
+				fib();
+		}
+		std::cout<<std::endl;
+	}
 }
+	
 
-
-
-void ThreadFunction(int number){
-mu.lock();
-std::cout<<"Hi,world from "<<number<<"-th thread id: "<<std::this_thread::get_id()<<std::endl;
-mu.unlock();
-}
 
 int main() {
-	std::cout<<std::endl;
 	Fib fib;
 
-	for(int i=0;i<10;i++){
-		std::cout<<fib()<<",";
+	for(int i=0;i<N;i++){
+		std::cout<<fib()<<" ";
 	}
 	std::cout<<std::endl;
 	std::cout<<std::endl;
 	
-std::cout<<std::endl;
-std::cout<<std::endl;
-
-std::cout<<"Hi,world from main thread"<<std::endl;
-std::cout<<" Main thread id: "<<std::this_thread::get_id()<<std::endl;
-std::cout<<"we have "<<hardware_threads<<" processors"<<std::endl;
-
-std::thread Evenfib(evenfib,10);
-std::thread thFirst(ThreadFunction,1);
-mu.lock();
-std::cout<<" First thread id: "<<thFirst.get_id()<<std::endl;
-mu.unlock();
-std::thread thSecond(ThreadFunction,2);
-mu.lock();
-std::cout<<" Second thread id: "<<thSecond.get_id()<<std::endl;
-mu.unlock();
-std::thread thThird(ThreadFunction,3);
-mu.lock();
-std::cout<<" Third thread id: "<<thThird.get_id()<<std::endl;
-mu.unlock();
-
-Evenfib.join();
-
-thFirst.join();
-thThird.join();
-thSecond.join();
+	mu.lock();
+	std::thread thSecond(ZhupTaq, 1);
+	mu.unlock();
+	mu.lock();
+	std::thread thFirst(ZhupTaq,2);
+	mu.unlock();
+	
+	thFirst.join();
+	thSecond.join();
 
 return 0;
 }
-
